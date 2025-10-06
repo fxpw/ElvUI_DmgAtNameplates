30951358c553b33b908a932de74548bf43ee1967
fxpw <fxpw@outlook.com>
2025-10-06 10:33:38 +0300

    ru loc

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,9 +1,85 @@
 local E = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local L = E.Libs.ACL:NewLocale("ElvUI", "enUS", true, true)
 if not L then return end
-L["common"] = true
-L["commondesc"] = true
-L["onorof"] = true
-L["onorofdesc"] = true
-L["Reflected"] = true
-L["Resisted"] = true
+
+-------------- text desc
+L["Reflected"] = "Отражено"
+L["Resisted"] = "Резист"
+
+L["Absorb"] = "Поглощено"
+L["Block"] = "Блок"
+L["Deflect"] = "Отражение"
+L["Dodge"] = "Уворот"
+L["Evade"] = "Избегание"
+L["Immune"] = "Иммунтитет"
+L["Miss"] = "Промах"
+L["Parry"] = "Паррирование"
+
+------------- options desc
+L["common"] = "Общие"
+L["commondesc"] = "Общие настройки"
+L["onorof"] = "Включить?"
+L["onorofdesc"] = "Включает или выключает аддон"
+L["font"] = "Шрифт"
+L["fontSize"] = "Размер шрифта"
+L["Font Outline"] = "Обводка"
+L["NONE"] = "Нет"
+L["sicon"] = "Показывать иконку"
+L["offtarget"] = "Разный размер для офф таргетов"
+L["otfSize"] = "Размер"
+L["fontAlpha"] = "Прозрачность"
+L["showFromAnotherPlayer"] = "Показывать текст не игрока"
+
+L["textformat"] = "Формат текста"
+L["kkk"] = "1,2k  <-- cокращать на тысячи"
+L["csep"] = "1,200  <-- с запятой"
+L["none"] = "1200  <-- просто текст"
+
+------------- small hits
+L["SmallHits"] = "Промежуточный урон"
+L["SmallHitsdesc"] = "Показывает промежуточный урон дот"
+L["SmallHitsScale"] = "Скалирование промежуточного урона"
+L["SmallHitsHidedesc"] = "Скалирование промежуточного урона"
+L["SmallHitsHide"] = "Прятать промежуточный урон"
+L["SmallHitsHidedesc"] = "Тут может быть ваша реклама"
+-------------------- animations Dmg
+L["AnimationDmg"] = "Анимации урона"
+L["autoAttackPlusCritAnimation"] = "АА + крит"
+L["autoAttack"] = "АА"
+L["crit"] = "Крит"
+L["commonDMGAnimation"] = "Обычный урон"
+------------------- animating heal
+L["noHealCrit"] = "Обычное исцеление"
+L["AnimationHeal"] = "Анимация исцеления"
+L["healColor"] = "Цвет текста исцеления"
+L["showOverHeal"] ="Показывать оверхил"
+L["shwrhlldesc"] ="Показывать оверхил?"
+
+L["Vertical Up"] = "Поднятие вверх"
+L["Vertical Down"] = "Опускается вниз"
+L["Fountain"] = "Фонтан"
+L["Rainfall"] = "Капельки дожджа"
+L["Disabled"] = "Отключена"
+----------------dmg
+L["damageText"] = "Урон персонажа"
+----------------heal
+L["healText"] = "Исцеление персонажа"
+----------------player to target dmg
+L["playerToTargetDamageText"] = "Урон ОТ персонажа"
+L["pttdtdesc"] = "Урон ОТ персонажа"
+----------------player to target heal
+L["playerToTargetHealText"] = "Исцеление ОТ персонажа"
+L["ptthtdesc"] = "Исцеление ОТ персонажа"
+----------------target to player dmg
+L["targetToPlayerDamageText"] = "Урон В персонажа"
+L["ttpdtdesc"] = "Урон В персонажа"
+L["showDmgToFriend"] = "Показывать урон в Дружественные юниты"
+L["showDmgToEnemy"] = "Показывать урон во Враждебные юниты"
+----------------target to player heal
+L["targetToPlayerHealText"] = "Исцеление В персонажа"
+L["ttphtdesc"] = "Исцеление В персонажа"
+-----------------pet text
+L["petToTargetDamageText"] = "Урон ОТ пета"
+L["petttdtdesc"] = "Урон ОТ пета"
+L["petToTargetHealText"] = "Хил От пета"
+L["pettthtdesc"] = "Хил От пета"
